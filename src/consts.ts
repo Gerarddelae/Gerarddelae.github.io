4291ee90339222d6fa78e708b5457e972bbf227f
Gerardo de la e <gerardmoe@hotmail.com>
2025-04-16 18:01:20 -0500

    first eddit

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'Gerardo';
+export const SITE_TAGLINE = 'Portfolio'; // site tagline
+export const SITE_DESCRIPTION = 'Portfolio'; // site meta description for SEO
--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -12,54 +12,16 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    name: 'Devaradise.com',
-    description: 'A blog that sharing web development resources and tutorials',
-    demoLink: 'https://devaradise.com',
-    tags: ['Blog']
+    name: 'HOTEL SPA',
+    description: 'Hotel SPA is a single-page application (SPA) designed for small hotels looking to efficiently manage guests, bookings, rooms, and other essential operations. The platform provides an intuitive interface for hotel staff to handle daily operations while maintaining a centralized database of all hotel-related information.',
+    demoLink: 'https://github.com/Gerarddelae/hotel-spa',
+    tags: ['SaaS', 'JS']
   },
   {
-    name: 'Sellercraft App',
-    description: 'An Ecommerce omnichannel platform in Southeast Asia',
-    demoLink: 'https://sellercraft.co',
+    name: 'SQL AGENT',
+    description: "The SQL Query Agent is a powerful, AI-assisted tool designed to streamline and optimize database interactions. It allows users to generate, analyze, and refine SQL queries effortlessly using natural language processing (NLP). Whether you're a developer, data analyst, or business user, this tool simplifies complex database operations while ensuring accuracy and efficiency.",
+    demoLink: 'https://github.com/Gerarddelae/Hotel-SQL-Agent',
     demoLinkRel: 'nofollow noopener noreferrer',
-    tags: ['ECommerce', 'Saas']
-  },
-  {
-    name: 'Gaji.id App',
-    description: 'Payroll and HR Management Information System',
-    demoLink: 'https://sellercraft.co',
-    demoLinkRel: 'nofollow noopener noreferrer',
-    tags: ['HRIS', 'Saas']
-  },
-  {
-    ...(await getRepositoryDetails('devaradise/paradise-ui')),
-    name: 'Paradise UI',
-    demoLink: 'https://paradise-ui.com',
-    postLink: 'https://devaradise.com/how-i-build-paradise-ui-react-component-library/',
-    tags: ['React', 'UI Library']
-  },
-  {
-    ...(await getRepositoryDetails('syakirurahman/react-lab')),
-    name: 'React Lab',
-    demoLink: 'https://devaradise.com/lab/react/',
-    tags: ['React']
-  },
-  {
-    ...(await getRepositoryDetails('syakirurahman/pokemon-catcher')),
-    name: 'Pokemon Catcher',
-    demoLink: 'https://pokemon-catcher-18636.web.app/',
-    tags: ['Hobby']
-  },
-  {
-    ...(await getRepositoryDetails('syakirurahman/movie-nominations')),
-    name: 'Movie Nominations',
-    demoLink: 'https://movie-nominations-c21c3.web.app/',
-    tags: ['Hobby']
-  },
-  {
-    ...(await getRepositoryDetails('syakirurahman/organization-tree')),
-    name: 'Organization tree',
-    demoLink: 'https://organization-tree-2a446.web.app/',
-    tags: ['Hobby']
+    tags: ['AI', 'Saas', 'Agent', 'Python']
   }
 ]
